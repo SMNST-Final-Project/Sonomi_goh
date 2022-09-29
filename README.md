d90e38af0fd7fb18c5f0507396fee7c83483c8c1
green2050 <go@gail.com>
2022-09-29 15:03:58 +0200

    readMe and terms&conditions added

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
-##SMNST
+<h1 align="center">Sonomi App Frontend</h1>
+<p align="center">
+check our <a href="https://github.com/SMNST-Final-Project/deployready/tree/main/server" target="_blank">Backend</a> or our <a href="" target="_blank">live app</a>
+</p>
+
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [About](#about)  
+- [Features](#features)
+- [Technologies](#technologies)
+- [Team](#team)
+- [Links](#links)
+
+<!-- ABOUT -->
+
+## Overview
+
+
+Our Final Project for the 1 year Full Stack Web development course at [DCI Digital Career Institute gGmbH](https://digitalcareerinstitute.org/de/).
+
+The Project take place from the 20th of August 2022 to the 11th October, 2022.
+
+## About
+
+> "The natural healing force within each one of us The Greatest Force in getting well. Our food should be our medicine. Our medicine should be our food." Hippocrates
+
+Our Darling Project "Sonomi, garden of hope" by all means shows our enthusiasm towards Mother Nature and all the resources it has offered Us as Humans to keep our heath and wellbeing at an optimum level.
+
+One of our team member has floated this idea of working on an application where people not only can buy Natural, Holistic, Chemical free products but also it provides a place to be in a community of like minded people interacting and exchanging ideas.
+
+
+
+
+
+
+## What is the idea?
+  - target users
+  - brief description (one paragraph)
+  - use cases
+  - What does it solve?
+  - ...
+
+## Design
+  - Color/Palette
+  - Layout
+  - Target Devices (mobile, desktop, ...)
+  - Mockups
+  - Wire frames
+  - Fonts
+  - ...
+
+## Structure and Software Architecture
+  - Fullstack
+    - Frontend
+    - Backend
+      - REST-API
+        - routes
+    - Database
+      - Seeding with dev/test data
+  - Data-Structure (potentially)
+  - flow charts
+  - ...
+
+## Technologies
+  - foreign APIs
+  - special software packages/modules
+  - ...
+
+
+# A MERN STACK FULL Application
+
+**Description
+
+This repository contains a project containing ReactJs Libraries. A series of ReactJs projects to learn React, Axios.
+
+
+### Libraries used
+* styled-components
+* axios
+* react-scripts
+* emoji-picker-react
+
+
+### 
+
+
+### Later
+* working on Design
+
+
+
+### LogIn
+#Basic button
+The Button comes with three variants: text (default), contained, and outlined.
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+
+
+## Material
+*Wrapped labels
+*Tabs
+*drawer
+*snackbar
+
+
+###Euro Є
+
+
+
+### 
+
+
+
+### 
+
+
+
+### 
+
+
+### 
--- client/src/components/TermsConditions/TermsConditions.jsx
+++ client/src/components/TermsConditions/TermsConditions.jsx
@@ -1,8 +1,84 @@
 
 import React from 'react'
+import styled from "styled-components";
+import { mobile } from "../../responsive.js";
+
+const Container = styled.div`
+   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px;
+  line-height: 2rem;
+  text-align: justify;
+  letter-spacing: 3px;
+  font-size: large;
+  font-weight: 700;
+  border: 1px solid gray;
+  padding: 8px;
+  background-color: #ddc7c7;
+  ${mobile({ flexDirection: "column" })}
+`;
 
 export const TermsConditions = () => {
   return (
-    <h1>TermsConditions</h1>
+   <> 
+   <h1 style={{marginBottom:"10px", marginLeft:"800px", marginTop:"20px", fontSize:"50px"}}>Terms And Conditions</h1>
+   <Container>
+   
+   <p> SONOMI Terms and Conditions Policy
+Welcome to SONOMI These terms and conditions outline the rules and regulations for the use of SONOMI’s Website.
+
+SONOMI is located at: , Germany.
+
+By accessing this website we assume you accept these terms and conditions in full. Do not continue to use SONOMI’s website if you do not accept all of the terms and conditions stated on this page.
+
+The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and any or all Agreements: Client, You and Your refers to you, the person accessing this website and accepting the Company’s terms and conditions. The Company, Ourselves, We, Our and Us, refers to our Company. Party, Parties, or Us, refers to both the Client and ourselves, or either the Client or ourselves.
+
+All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner, whether by formal meetings of a fixed duration, or any other means, for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services/products, in accordance with and subject to, prevailing law of GERMANY.
+
+Any use of the above terminology or other words in the singular, plural, capitalisation and/or he/she or they, are taken as interchangeable and therefore as referring to same.
+
+All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner, whether by formal meetings of a fixed duration, or any other means, for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services/products, in accordance with and subject to, prevailing law of GERMANY.
+
+Cookies
+
+We employ the use of cookies. By using SONOMI’s website you consent to the use of cookies in accordance with SONOMI’s privacy policy. Most of the modern day interactive websites use cookies to enable us to retrieve user details for each visit.
+
+Cookies are used in some areas of our site to enable the functionality of this area and ease of use for those people visiting. Some of our affiliate / advertising partners may also use cookies.
+
+License
+
+Unless otherwise stated, SONOMI and/or its licensors own the intellectual property rights for all material on SONOMI.
+
+All intellectual property rights are reserved. You may view and/or print pages from (Add URL) for your own personal use subject to restrictions set in these terms and conditions.
+
+You must not:
+
+Republish material from Our Website.
+Sell, rent or sub-license material from SONOMI.
+Reproduce, duplicate or copy material from SONOMI.
+Redistribute content from SONOMI (unless content is specifically made for redistribution).
+Disclaimer
+
+To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website (including, without limitation, any warranties implied by law in respect of satisfactory quality, fitness for purpose and/or the use of reasonable care and skill).
+
+Nothing in this disclaimer will:
+
+Limit or exclude our or your liability for death or personal injury resulting from negligence.
+Limit or exclude our or your liability for fraud or fraudulent misrepresentation.
+Limit any of our or your liabilities in any way that is not permitted under applicable law.
+Or exclude any of our or your liabilities that may not be excluded under applicable law.
+The limitations and exclusions of liability set out in this Section and elsewhere in this disclaimer:
+
+are subject to the preceding paragraph; and
+govern all liabilities arising under the disclaimer or in relation to the subject matter of this disclaimer, including liabilities that arise in contract, tort (including negligence) and for breach of statutory duty.
+To the extent that the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.
+
+</p>
+</Container>
+    
+    </> 
   )
 }
+
