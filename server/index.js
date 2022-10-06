2a8f24a097fc7ba95624e6147495c7f250b1b731
green2050 <go@gail.com>
2022-10-06 13:25:47 +0200

    heroku added

--- server/index.js
+++ server/index.js
@@ -1,25 +1,45 @@
 import express from 'express'; 
 import bodyParser from 'body-parser';
-import cors from 'cors'; 
-import morgan from 'morgan'; 
 
 import dotenv from 'dotenv'; 
 dotenv.config(); 
 
+import cors from 'cors'; 
+import morgan from 'morgan';
+
+
+const app = express(); 
+const PORT =  5000
+
+//path for heroku
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+//db connect
 import connectDB from './config/db.js';
 import errorHandler from './middleware/errorHandler.js'
 
+
+// it will get the name of my current running file that is server.js
+
+const __filename = fileURLToPath(import.meta.url);
+
+// server directory
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, "../client/build")))
+
+connectDB(); 
+app.use(cors()); 
+
+//routes
 import usersRoutes from './routes/users.js'; 
 import productsRoutes from './routes/products.js'; 
 import cartRoutes from './routes/cart.js';
 import ordersRoutes from './routes/orders.js'
 import paymentRoutes from './routes/checkout-session.js'; 
 
-const app = express(); 
-const PORT =  5000
-
-connectDB(); 
-app.use(cors()); 
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -32,9 +52,12 @@ app.use('/cart', cartRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/payment', paymentRoutes); 
 
-
-
+//error handlers
 app.use(errorHandler)
 
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html" ))
+})
 
 app.listen(PORT, () => console.log(`server up and running on port ${PORT}`))
