26f0d3c1bd5641decf7f84a3276a5c4c120172c5
green2050 <go@gail.com>
2022-10-10 17:04:23 +0200

    changes in team component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -79,7 +79,7 @@ export const Navbar = (props) => {
 
    <>
 
-      { location.pathname === "/login" || location.pathname === "/signup" || location.pathname === "/pay" ? null :
+      { location.pathname === "/login" || location.pathname === "/signup" || location.pathname === "/pay" || location.pathname === "/team" ? null :
 
     <>
       <Container>
